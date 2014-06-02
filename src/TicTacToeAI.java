ac623427fec09f37154eebbac5e3c23f09f6f39c
Pi3th0n <Chasemansell@gmail.com>
2014-06-01 21:16:59 -0400

    Final Changes

--- src/TicTacToeAI.java
+++ src/TicTacToeAI.java
@@ -2,11 +2,12 @@ import java.util.ArrayList;
 
 public class TicTacToeAI{
 
-    public ArrayList<MoveList> memory;
-    public ArrayList<Integer> recentMoveInts;
-    public ArrayList<MoveList> recentMoveLists;
-    public ArrayList<Integer> recentMoveRotations;
+    public ArrayList<MoveList> memory;	// The ArrayList that holds the MoveLists for every game the AI has ever seen
+    public ArrayList<Integer> recentMoveInts;    // Remembers which move was made each turn
+    public ArrayList<MoveList> recentMoveLists;  // Remembers which moveLists were used this game
+    public ArrayList<Integer> recentMoveRotations;  // 
 
+    
     public TicTacToeAI(){
         memory = new ArrayList<MoveList>();
         recentMoveInts = new ArrayList<Integer>();
@@ -14,40 +15,49 @@ public class TicTacToeAI{
         recentMoveRotations = new ArrayList<Integer>();
     }
 
+   /**
+    * Returns and int (0 through 8) representing the location the AI has decided to move
+    * 
+    * @param _gameState A 9-character string representing the tic-tac-toe board
+    * @return an int between 0 and 8, inclusive
+    */
     public int makeMove(String _gameState) {
         // Search memory for the same gameState
-        int dolan;
+        // dolan will remember where the applicable moveList is
+    	int dolan;
+        
         if (memory.size() < 1) {
+        	// -1 means that the applicable moveList does not exist, which it can't because memory is empty
             dolan = -1;
+            
         } else {
             dolan = -1;
+            // We'll assume that the moveList isn't in memory, but if we find it, we'll tell dolan
             for (int i = 0; i < memory.size(); i++) {
                 int temp = memory.get(i).compareGameState(_gameState);
                 if (temp != 0) {
+                	// Oh, we found it!
                     dolan = i;
                     recentMoveRotations.add(temp);
-                    if (temp == 1){
-//                        System.out.println("I've seen that board before");
-                    } else {
-//                        System.out.println("I've seen a board similar to that one before");
-                    }
                 }
             }
         }
-        // Add the move to memory
+        
+        // There is a similar moveList somewhere in memory
         if (dolan >= 0) {
             int sam = memory.get(dolan).getMove();
             recentMoveInts.add(sam);
             recentMoveLists.add(memory.get(dolan)); 
             return rotate(sam, recentMoveRotations.get(recentMoveRotations.size()-1));
-        } else {
-//            System.out.println("I've never seen that board before");
-            memory.add(new MoveList(_gameState));
+        
+        } else {  // There was not a similar moveList somewhere in memory
+            // Add the move to Memory
+        	memory.add(new MoveList(_gameState));
             recentMoveLists.add(new MoveList(_gameState));
             recentMoveInts.add(memory.get(memory.size()-1).getMove());
+            // Then return the move most recently added to memory
+            return recentMoveInts.get(recentMoveInts.size()-1);
         }
-        // Return the move most recently added to memory
-        return recentMoveInts.get(recentMoveInts.size()-1);
     }
 
    /**
@@ -79,18 +89,28 @@ public class TicTacToeAI{
     }
     
 
+    // This method makes sure the AI learns from previous games.  A gameResult > 1 increases the probability
+    // that the AI will make similar moves in the future, while a gameResult < 1 decreases the probability that
+    // the AI will make similar moves in the future.
     public void learn(double gameResult){
-        for (int i = 0; i < recentMoveLists.size(); i++){
-            for (int k = 0; k < memory.size(); k++){
-                if (memory.get(k).gameState.equals(recentMoveLists.get(i).gameState)){
-                    if (i == recentMoveLists.size()-1){
+    	
+        for (int i = 0; i < recentMoveLists.size(); i++)
+        {	// Iterate through our list of moveLists used this game
+            for (int k = 0; k < memory.size(); k++)
+            {   // Search memory to find it
+                if (memory.get(k).gameState.equals(recentMoveLists.get(i).gameState))
+                {
+                    if (i == recentMoveLists.size()-1)
+                    {   // If this was the last move of the game, it probably directly affected whether you won or lost 
                         memory.get(k).probabilityChange(recentMoveInts.get(i), gameResult*gameResult);
-                    } else if (i == 0) {
+                    } 
+                    else if (i == 0) 
+                    {   // The first move of the game probably had less effect on whether you won or lost
                         memory.get(k).probabilityChange(recentMoveInts.get(i), Math.sqrt(gameResult));
-                        
-                    } else {
+                    } 
+                    else 
+                    {   // Otherwise, it slightly affected whether you won or lost
                         memory.get(k).probabilityChange(recentMoveInts.get(i), gameResult);
-                    
                     }
                 }
             }
--- src/TicTacToeGui.java
+++ src/TicTacToeGui.java
@@ -2,50 +2,548 @@ import javax.swing.JFrame;
 import javax.swing.JMenuBar;
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
+import javax.swing.JButton;
+import javax.swing.JOptionPane;
 
+import java.util.ArrayList;
+
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 
 public class TicTacToeGui implements Runnable{
 
-	JMenuBar firstHill;
-	
-	// Constructors up top, the rest should be sorted alphabetically by method name
-	public TicTacToeGui(){
+	JMenuBar firstHill;  // Our JMenuBar
+	ArrayList<JButton> buttList;  // This ArrayList holds the 9 buttons that the game is played on
+	int turn; // What turn is it?
+	JFrame chase; // Our JFrame
+	int gameType; // Controls wether the game is pvp or pvAI
+	TicTacToeAI Xander;  // Xander is our X bot
+	TicTacToeAI Olivia;  // Olivia is our O bot
+	TicTacToeAI practiceBot; // This guy is the dummy they practice against
+
+	public TicTacToeGui()
+	{  // Initializing Variables
 		firstHill = new JMenuBar();
+		buttList = new ArrayList<JButton>();
+		chase = new JFrame();
+		turn = 0;
+		gameType = 0;
+		Xander = new TicTacToeAI();
+		Olivia = new TicTacToeAI();
+		practiceBot = new TicTacToeAI();
+		for( int i = 0; i < 9; i++)
+		{ // Making all 9 buttons, initially disabled
+			JButton butt = new JButton(" ");
+			butt.setEnabled(false);
+			butt.addActionListener(new ActionListener(){
+				public void actionPerformed(ActionEvent e){
+					Object o = e.getSource();
+					if(o instanceof JButton)
+					{
+						JButton sourceButton = (JButton)e.getSource();
+						buttonClicked(sourceButton);
+					}
+				}
+			});
+			buttList.add(butt);
+		}
 	}
-	
-	public static void main(String[] args){
-		TicTacToeGui stephen = new TicTacToeGui();
-		javax.swing.SwingUtilities.invokeLater(stephen);
-		
+
+	public static void main(String[] args)
+	{
+			TicTacToeGui stephen = new TicTacToeGui();
+			javax.swing.SwingUtilities.invokeLater(stephen);
 	}
-	
-	public void makeFileMenu(){
-		JMenu fileMenu = new JMenu("File");
+
+	public void makeFileMenu()
+	{
+        JMenu menu = new JMenu("Menu");
         
         JMenuItem quitMI = new JMenuItem("Quit");
-
         quitMI.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e){
                 System.exit(0);
             }
-        });        
+        });
         
-        fileMenu.add(quitMI);
-        firstHill.add(fileMenu);
+        JMenuItem trainAI = new JMenuItem("Speed-train the AI");
+        trainAI.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e){
+               trainAI();
+            }
+        });       
+        
+        JMenuItem newPlayerMI = new JMenuItem("New Game vs. Player");
+        newPlayerMI.addActionListener(new ActionListener(){
+          public void actionPerformed(ActionEvent e){
+            newGame(1);
+          }
+        });
+        
+        JMenuItem newAIXMI = new JMenuItem("New Game vs. AI 'Olivia'");        
+        newAIXMI.addActionListener(new ActionListener(){
+          public void actionPerformed(ActionEvent e){
+        	  newGame(2);
+           }
+        });
+        
+        JMenuItem newAIOMI = new JMenuItem("New Game vs. AI 'Xander'");        
+        newAIOMI.addActionListener(new ActionListener(){
+          public void actionPerformed(ActionEvent e){
+        	  newGame(3);
+           }
+        });
+        menu.add(newPlayerMI);
+        menu.add(newAIXMI);
+        menu.add(newAIOMI);
+        menu.add(trainAI);
+        menu.add(quitMI);
+        firstHill.add(menu);
 	}
-	
+
 	public void run(){
-		JFrame chase = new JFrame();
 		chase.setSize(640,480);
 		chase.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
+		chase.getContentPane().setLayout(new GridLayout(3, 3));
 		makeFileMenu();
 		chase.setJMenuBar(firstHill);
-		
-		
+		for(int i = 0; i < buttList.size(); i++)
+		{
+			chase.getContentPane().add(buttList.get(i));
+		}
+
 		chase.setVisible(true);
+
+	}
+
+	public void buttonClicked(JButton jbutt)
+	{
+		// We don't want buttons to do anything while the AI is training, or when a game hasn't started
+		if (gameType == 4 || gameType == 0)
+		{
+			return;
+		}
+		
+		// On even turns, X goes
+		if(turn%2 == 0)
+		{
+			jbutt.setText("X");
+			jbutt.setEnabled(false);
+		}
+		
+		// On odd turns, O goes
+		else if(turn%2 == 1)
+		{
+			jbutt.setText("O");
+			jbutt.setEnabled(false);
+		}
+		
+		// Check for win (Since the player just moved, we know that if someone won, it must have been the player)
+		int winner = checkForWin();
+		
+		// Initialize response, and make sure it's not YES_OPTION or NO_OPTION
+		int response = 69;
+		
+		// O wins
+		if(winner == 1)
+		{
+			response = JOptionPane.showConfirmDialog(chase, "O has Won! Would you like to play again?", 
+					"O has won!", JOptionPane.YES_NO_OPTION);
+			// If this is a bot game, we want the bots to learn from this loss
+			if (gameType == 2 || gameType == 3) { Xander.learn(0.5); }
+		}
+		
+		// X wins
+		if(winner == -1)
+		{
+			response = JOptionPane.showConfirmDialog(chase, "X has Won! Would you like to play again?", 
+					"X has won!", JOptionPane.YES_NO_OPTION);
+			// If this is a bot game, we want the bots to learn from this loss
+			if (gameType ==2 || gameType == 3) { Olivia.learn(0.5); }
+		}
+		
+		// Tie
+		if(winner == 2)
+		{
+			response = JOptionPane.showConfirmDialog(chase, "It is a tie! Would you like to play again?", 
+					"You have tied!", JOptionPane.YES_NO_OPTION);
+			// Draws are better than losing, so we'll have the appropriate bot learn, if this was a bot game.
+			if (gameType == 2) { Olivia.learn(1.5); }
+			else if (gameType == 3) { Xander.learn(1.5); }
+		}
+		
+		// Play again?
+		if(response == JOptionPane.YES_OPTION)
+		{
+			newGame(gameType);
+			return;
+		}
+		
+		if(response == JOptionPane.NO_OPTION)
+		{
+			System.exit(1); 
+		}
+		turn++;
+		
+		// If this is a pvp game, do nothing else
+		// If this was a bot game, it is now the bot's turn
+		if(gameType == 2 || gameType == 3)
+		{
+			
+			if (turn%2 == 0)
+			{
+				// Xander is our X bot, so he'll make moves for X
+				jbutt = buttList.get(Xander.makeMove(arrayToString()));
+				jbutt.setText("X");
+				jbutt.setEnabled(false);
+			}
+			else if (turn%2 == 1)
+			{
+				// Olivia is our O bot, so she'll make moves for O
+				jbutt = buttList.get(Olivia.makeMove(arrayToString()));
+				jbutt.setText("O");
+				jbutt.setEnabled(false);
+			}
+			turn++;
+			// Now we'll check for win again, but now we know that if someone won, it was a bot
+			winner = checkForWin();
+			response = 69;
+			
+			// O Wins!
+			if(winner == 1)
+			{
+				response = JOptionPane.showConfirmDialog(chase, "O has Won! Would you like to play again?", 
+						"O has won!", JOptionPane.YES_NO_OPTION);
+				// Winning is good!
+				Olivia.learn(10);
+			}
+			
+			// X Wins!
+			if(winner == -1)
+			{
+				response = JOptionPane.showConfirmDialog(chase, "X has Won! Would you like to play again?", 
+						"X has won!", JOptionPane.YES_NO_OPTION);
+				// Winning is good!
+				Xander.learn(10);
+			}
+
+			// Tie
+			if(winner == 2)
+			{
+				response = JOptionPane.showConfirmDialog(chase, "It is a tie! Would you like to play again?", 
+						"You have tied!", JOptionPane.YES_NO_OPTION);
+				Xander.learn(1.0);
+				Olivia.learn(1.0);
+			}
+			
+			// Play again?
+			if(response == JOptionPane.YES_OPTION)
+			{
+				newGame(gameType);
+				return;
+			}
+			
+			if(response == JOptionPane.NO_OPTION)
+			{
+				System.exit(1); 
+			}
+		}
+	}
+ 
+	// This method basically just brute force checks every win combination to see if someone won
+	// Returns -1 if X wins, 1 if O wins, 2 for a tie and 0 if the game is not yet over.
+	public int checkForWin()
+	{
+		String isItWinning = arrayToString();
+		if (isItWinning.length() != 9){
+			throw new IllegalArgumentException("String isItWinning is of incorrect length");
+		} else if (!isItWinning.replaceAll("X", "").replaceAll("O", "").replaceAll(" ", "").equals("")){
+            throw new IllegalArgumentException("String isItWinning contains invalid characters");
+        }
+
+        if (isItWinning.charAt(0) == isItWinning.charAt(1) && isItWinning.charAt(0) == isItWinning.charAt(2)) {
+            if (isItWinning.charAt(0) == 'X'){
+                return -1;
+            } else if (isItWinning.charAt(0) == 'O') {
+                return 1;
+            }
+
+        } else if (isItWinning.charAt(3) == isItWinning.charAt(4) && isItWinning.charAt(3) == isItWinning.charAt(5)){
+            if (isItWinning.charAt(3) == 'X'){
+                return -1;
+            } else if (isItWinning.charAt(3) == 'O') {
+                return 1;
+            }
+        
+        } else if (isItWinning.charAt(6) == isItWinning.charAt(7) && isItWinning.charAt(6) == isItWinning.charAt(8)){
+            if (isItWinning.charAt(6) == 'X'){
+                return -1;
+            } else if (isItWinning.charAt(6) == 'O') {
+                return 1;
+            }
+        
+        } else if (isItWinning.charAt(0) == isItWinning.charAt(3) && isItWinning.charAt(0) == isItWinning.charAt(6)){
+            if (isItWinning.charAt(0) == 'X'){
+                return -1;
+            } else if (isItWinning.charAt(0) == 'O') {
+                return 1;
+            }
+
+        } else if (isItWinning.charAt(1) == isItWinning.charAt(4) && isItWinning.charAt(1) == isItWinning.charAt(7)){
+            if (isItWinning.charAt(1) == 'X'){
+                return -1;
+            } else if (isItWinning.charAt(1) == 'O') {
+                return 1;
+            }
+
+        } else if (isItWinning.charAt(2) == isItWinning.charAt(5) && isItWinning.charAt(2) == isItWinning.charAt(8)){
+            if (isItWinning.charAt(2) == 'X'){
+                return -1;
+            } else if (isItWinning.charAt(2) == 'O') {
+                return 1;
+            }
+
+        } else if (isItWinning.charAt(0) == isItWinning.charAt(4) && isItWinning.charAt(0) == isItWinning.charAt(8)){
+            if (isItWinning.charAt(0) == 'X'){
+                return -1;
+            } else if (isItWinning.charAt(0) == 'O') {
+                return 1;
+            }
+
+        } else if (isItWinning.charAt(2) == isItWinning.charAt(4) && isItWinning.charAt(2) == isItWinning.charAt(6)){
+            if (isItWinning.charAt(2) == 'X'){
+                return -1;
+            } else if (isItWinning.charAt(2) == 'O') {
+                return 1;
+            }
+
+        } else if (isItWinning.replaceAll(" ", "").length() == 9) {
+            return 2;
+        }
+        return 0;
+    }
+ 
+	public void newGame(int n)
+	{
+		// Clear the board and enable all the buttons
+		for(int i = 0; i < buttList.size(); i++)
+        {
+			turn = 0;
+			buttList.get(i).setText(" ");
+			buttList.get(i).setEnabled(true);
+        }
+		
+		// Set the gameType to whatever it's supposed to be
+		gameType = n;
+		
+		// GameType three is player vs AI but the player goes second.  We'll go ahead and make the AI's first move here
+		if (n == 3)
+		{
+			JButton jbutt = buttList.get(Xander.makeMove(arrayToString()));
+			jbutt.setText("X");
+			jbutt.setEnabled(false);
+			turn++;
+		}
+	}
+ 
+	// This method turns the board into a string that the AI can use
+	public String arrayToString()
+	{
+		
+		String winning = new String("");
+		for(int i = 0; i < buttList.size(); i++)
+		{
+			winning += (buttList.get(i).getText());
+		}
+		return winning;
+	}
+ 
+	// This method runs 150 to help the AI learn to play TicTacToe
+	public void trainAI()
+	{
+		newGame(4);
+		Xander.recentMoveInts.clear();
+		Xander.recentMoveLists.clear();
+		Xander.recentMoveRotations.clear();
+		Olivia.recentMoveInts.clear();
+		Olivia.recentMoveLists.clear();
+		Olivia.recentMoveRotations.clear();
+		
+		for (int i = 0; i < 50; i++)
+		{
+			// For these 50 games, We'll train Xander
+			boolean gameOver = false;
+			while (!gameOver)
+			{
+								
+				// Xander's Turn
+				JButton jbutt = buttList.get(Xander.makeMove(arrayToString()));
+				jbutt.setText("X");
+				jbutt.setEnabled(false);
+				turn++;
+			
+				int winner = checkForWin();
+				
+				// X Wins!
+				if(winner == -1)
+				{
+					gameOver = true;
+					newGame(3);
+					Xander.learn(10.0);
+				}
+				
+				// Tie
+				if(winner == 2)
+				{
+					gameOver = true;
+					newGame(3);
+					Xander.learn(1.5);
+				}
+			
+				// Practice Bot's turn
+				jbutt = buttList.get(practiceBot.makeMove(arrayToString()));
+				jbutt.setText("O");
+				jbutt.setEnabled(false);
+				turn++;
+			
+				winner = checkForWin();
+				// O wins
+				if(winner == 1)
+				{
+					gameOver = true;
+					newGame(3);
+					Xander.learn(0.05);
+				}
+				// Tie
+				if(winner == 2)
+				{
+					gameOver = true;
+					newGame(3);
+					Xander.learn(1.5);
+				}
+			}
+		}
+		for (int i = 0; i < 50; i++)
+		{
+			// For these 50 games, we'll train Olivia
+			
+			boolean gameOver = false;
+			while (!gameOver)
+			{
+				
+				// Practice Bot's turn				
+				JButton	jbutt = buttList.get(practiceBot.makeMove(arrayToString()));
+				jbutt.setText("X");
+				jbutt.setEnabled(false);
+				turn++;
+			
+				int winner = checkForWin();
+				
+				// X wins!
+				if(winner == -1)
+				{
+					gameOver = true;
+					newGame(4);
+					Olivia.learn(0.05);
+				}
+				
+				// Tie
+				if(winner == 2)
+				{
+					gameOver = true;
+					newGame(4);
+					Olivia.learn(1.5);
+				}
+				
+				// Olivia's Turn
+				jbutt = buttList.get(Olivia.makeMove(arrayToString()));
+				jbutt.setText("O");
+				jbutt.setEnabled(false);
+				turn++;
+			
+				winner = checkForWin();
+				
+				// O wins!
+				if(winner == 1)
+				{
+					gameOver = true;
+					newGame(3);
+					Olivia.learn(10.0);
+				}
+				// Tie
+				if(winner == 2)
+				{
+					gameOver = true;
+					newGame(3);
+					Olivia.learn(1.5);
+				}
+			
+			}
+		}
+		for (int i = 0; i < 50; i++)
+		{
+			// For these 50 games, we'll have Xander and Olivia play against each other
+			
+			boolean gameOver = false;
+			while (!gameOver)
+			{
+				
+				// Xander's turn				
+				JButton	jbutt = buttList.get(Xander.makeMove(arrayToString()));
+				jbutt.setText("X");
+				jbutt.setEnabled(false);
+				turn++;
+			
+				int winner = checkForWin();
+				
+				// X wins!
+				if(winner == -1)
+				{
+					gameOver = true;
+					newGame(4);
+					Xander.learn(10.0);
+					Olivia.learn(0.05);
+				}
+				
+				// Tie
+				if(winner == 2)
+				{
+					gameOver = true;
+					newGame(4);
+					Xander.learn(1.5);
+					Olivia.learn(1.5);
+				}
+				
+				// Olivia's Turn
+				jbutt = buttList.get(Olivia.makeMove(arrayToString()));
+				jbutt.setText("O");
+				jbutt.setEnabled(false);
+				turn++;
+			
+				winner = checkForWin();
+				
+				// O wins!
+				if(winner == 1)
+				{
+					gameOver = true;
+					newGame(3);
+					Xander.learn(0.05);
+					Olivia.learn(10.0);
+				}
+				// Tie
+				if(winner == 2)
+				{
+					gameOver = true;
+					newGame(3);
+					Xander.learn(1.5);
+					Olivia.learn(1.5);
+				}
+			
+			}
+		}
+		newGame(0);
+		JOptionPane.showMessageDialog(chase,"Training Complete");
 	}
 }
